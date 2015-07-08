524bd1f62313143696051415d6d434ebd6844b04
avan <azeng97@gmail.com>
2015-07-08 17:37:59 +1000

    modified dupe.c to find if nearby

---
+++ dupe2.c
@@ -0,0 +1,23 @@
+//returns whether array contains duplicates
+//by Avan
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool containsNearbyDuplicate(int* nums, int numsSize, int k) {
+    int x = 0;
+    int y = 1;
+    while (x<numsSize){
+    	while (y<numsSize){
+    		if (nums[x]==nums[y] && abs(x-y)<=k){
+    			return true;
+    		} else {
+    			y++;
+    		}
+    	}
+    	x++;
+    	y = x+1;
+    }
+    return false;
+}
